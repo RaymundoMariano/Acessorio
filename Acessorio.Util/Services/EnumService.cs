a8461df1c248b18ce5717a182f00e0043e618a1e
Raymundo Mariano <gledisson@hotmail.com>
2021-07-05 08:53:18 -0300

    Novo serviço

---
+++ Acessorio.Util/Services/EnumService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Acessorio.Util.Services
+{
+    public static class EnumService<T> where T : Enum
+    {
+        public static IList<SelectOption> GetOptions<T>()
+        {
+            var tipo = typeof(T);
+            var result = Enum.GetNames(tipo).Select(
+                item => new SelectOption
+                {
+                    Value = (int)Enum.Parse(tipo, item),
+                    Description = GetDescription<T>(item)
+                }).ToList();
+            return result;
+        }
+
+        public static string GetDescription<T>(string enumDescription)
+        {
+            var descriptionAttribute = typeof(T)
+                .GetField(enumDescription)
+                .GetCustomAttributes(typeof(DescriptionAttribute), false)
+                .FirstOrDefault() as DescriptionAttribute;
+            return descriptionAttribute != null
+                ? descriptionAttribute.Description
+                : enumDescription;
+        }
+    }
+
+    public class SelectOption
+    {
+        public int Value { get; set; }
+        public string Description { get; set; }
+    }
+}
